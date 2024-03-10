1ca53daa183b78c72738792e60389c6a98abc706
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-03-10 11:47:00 +0300

    configure vitest with process.env

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths"; 
+import { loadEnv } from "vite";
+
+const env = loadEnv("", process.cwd(), "");
+Object.assign(process.env, env);
 
 export default defineConfig({
   test: {
     globals: true,
+    setupFiles: ["dotenv/config"],
   },
   plugins: [tsconfigPaths()],
 });
+
+
